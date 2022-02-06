193232c044f463ba1eab3f88e60d49dbf3442635
Gerald Annan <annang2@stanbic.com.gh>
2022-02-06 00:18:17 +0000

    should locate a left to right word in two line grid

--- test/word_search_test.exs
+++ test/word_search_test.exs
@@ -61,7 +61,6 @@ defmodule WordSearchTest do
     assert output == expected
   end
 
-  @tag :pending
   test "Should locate a left to right word in two line grid" do
     grid = """
     jefblpepre
