3fb821b4d6f14cee893d58aab8cf9e165e03b872
Gerald Annan <annang2@stanbic.com.gh>
2022-02-18 13:58:49 +0000

    solved word search problem

--- _build/test/lib/word_search/ebin/word_search.app
+++ _build/test/lib/word_search/ebin/word_search.app
@@ -1,6 +1,7 @@
 {application,word_search,
              [{applications,[kernel,stdlib,elixir,logger]},
               {description,"word_search"},
-              {modules,['Elixir.WordSearch','Elixir.WordSearch.Location']},
+              {modules,['Elixir.WordSearch','Elixir.WordSearch.Grid',
+                        'Elixir.WordSearch.Location']},
               {registered,[]},
               {vsn,"0.1.0"}]}.
--- lib/word_search.ex
+++ lib/word_search.ex
@@ -2,218 +2,140 @@ defmodule WordSearch do
   defmodule Location do
     defstruct [:from, :to]
 
-    @type t :: %Location{
+    @type t :: %__MODULE__{
             from: %{row: integer, column: integer},
             to: %{row: integer, column: integer}
           }
+
+    def new({x1, y1} = _from, {x2, y2} = _to) do
+      %__MODULE__{from: %{row: y1 + 1, column: x1 + 1}, to: %{row: y2 + 1, column: x2 + 1}}
+    end
   end
+
+  defmodule Grid do
+    defstruct [:grid, :rows, :cols]
+
+    def new(formatted) do
+      formatted_rows = formatted |> String.split("\n", trim: true)
+
+      grid =
+        for {row, y} <- formatted_rows |> Stream.with_index(),
+            {elem, x} <- to_charlist(row) |> Stream.with_index(),
+            into: %{},
+            do: {{x, y}, elem}
+
+      rows = length(formatted_rows)
+      cols = formatted_rows |> hd() |> to_charlist() |> length()
+
+      %__MODULE__{grid: grid, rows: rows, cols: cols}
+    end
+
+    @domain_directions ~w[right diagonal_right down diagonal_left]a
+
+    def generate_domains(%__MODULE__{} = grid, word) do
+      length = length(word)
+
+      for y <- 0..(grid.rows - 1),
+          x <- 0..(grid.cols - 1),
+          direction <- @domain_directions,
+          domain = generate_domain(grid, direction, x, y, length),
+          List.first(domain),
+          into: [],
+          do: domain
+    end
+
+    defp generate_domain(grid, :right, x, y, length)
+         when x + length <= grid.cols do
+      for x <- x..(x + length - 1),
+          into: [],
+          do: {x, y}
+    end
+
+    defp generate_domain(grid, :diagonal_right, x, y, length)
+         when x + length <= grid.cols and y + length <= grid.rows do
+      for d <- 0..(length - 1),
+          x = x + d,
+          y = y + d,
+          into: [],
+          do: {x, y}
+    end
+
+    defp generate_domain(grid, :down, x, y, length)
+         when y + length <= grid.rows do
+      for y <- y..(y + length - 1),
+          into: [],
+          do: {x, y}
+    end
+
+    defp generate_domain(grid, :diagonal_left, x, y, length)
+         when y + length <= grid.rows and x - length >= 0 do
+      for d <- 0..(length - 1),
+          x = x - d,
+          y = y + d,
+          into: [],
+          do: {x, y}
+    end
+
+    defp generate_domain(_, _, _, _, _), do: []
+
+    def fetch(%__MODULE__{} = grid, coord) do
+      grid.grid[coord]
+    end
+  end
+
+  @type solution :: %{String.t() => nil | Location.t()}
 
   @doc """
   Find the start and end positions of words in a grid of letters.
   Row and column positions are 1 indexed.
   """
-  @spec search(grid :: String.t(), words :: [String.t()]) :: %{String.t() => nil | Location.t()}
+  @spec search(grid :: String.t(), words :: [String.t()]) :: solution()
   def search(grid, words) do
-    flattened_grid_map =
-      String.replace(grid, "\n", " ")
-      |> String.trim()
-      |> String.split(" ")
-      |> Enum.with_index(fn element, row -> {row, element} end)
-      |> Enum.into(%{})
+    words = Enum.map(words, &to_charlist/1)
+    grid = Grid.new(grid)
+    domains = generate_domains(grid, words)
 
-    Enum.reduce(words, %{}, fn word, search_results ->
-      isHorizontal =
-        Enum.any?(flattened_grid_map, fn {_, element} ->
-          String.contains?(element, word) || String.contains?(element, String.reverse(word))
-        end)
+    find_words(grid, domains)
+  end
 
-      if !isHorizontal do
-        vertical =
-          Enum.reduce(1..map_size(flattened_grid_map), [], fn pos, arr ->
-            arr ++
-              [
-                Enum.reduce(flattened_grid_map, "", fn {_, element}, acc ->
-                  "#{acc}#{String.at(element, pos - 1)}"
-                end)
-              ]
-          end)
-          |> Enum.with_index(fn element, row -> {row, element} end)
-          |> Enum.into(%{})
+  def generate_domains(grid, words) do
+    for word <- words,
+        domains = Grid.generate_domains(grid, word),
+        into: %{},
+        do: {word, domains}
+  end
 
-        isVertical =
-          Enum.any?(vertical, fn {_, element} ->
-            String.contains?(element, word) || String.contains?(element, String.reverse(word))
-          end)
+  def find_words(grid, domains) do
+    for {word, domains} <- domains,
+        {:result, found} = {:result, find_word(grid, word, domains)},
+        word = to_string(word),
+        into: %{},
+        do: {word, found}
+  end
 
-        if !isVertical do
-          # Map.put_new(search_results, word, nil)
-          list = String.trim(grid) |> String.split("\n")
+  def find_word(grid, word, domains) do
+    domains
+    |> Stream.flat_map(fn domain ->
+      [
+        {:forward, domain},
+        {:reverse, domain}
+      ]
+    end)
+    |> Stream.map(fn
+      {:forward, domain} -> domain
+      {:reverse, domain} -> Enum.reverse(domain)
+    end)
+    |> Enum.find_value(fn domain ->
+      found? =
+        domain
+        |> Enum.map(&Grid.fetch(grid, &1))
+        |> Kernel.==(word)
 
-          %{i: _, vals: items} =
-            Enum.reduce(list, %{i: 0, vals: []}, fn _, %{i: i, vals: vals} ->
-              %{j: _, str: strval} =
-                Enum.reduce(list, %{j: i, str: ""}, fn element, %{j: j, str: str} ->
-                  %{j: j + 1, str: "#{str}#{String.at(element, j)}"}
-                end)
-
-              %{i: i + 1, vals: vals ++ [strval]}
-            end)
-
-          upper_grid =
-            items
-            |> Enum.with_index(fn element, col -> {col, element} end)
-            |> Enum.into(%{})
-
-          [isUpperHalf | [col | [element]]] =
-            Enum.reduce(upper_grid, [false, nil, nil], fn {index, element}, acc ->
-              if String.contains?(element, word),
-                do: [true, index + 1, element],
-                else: acc
-            end)
-
-          if isUpperHalf do
-            distance = String.length(element) - String.length(word)
-
-            Enum.reduce(0..distance, %{}, fn shift, acc ->
-              upper_bound = shift + (String.length(word) - 1)
-
-              if String.slice(element, shift..upper_bound) == word do
-                Map.put_new(acc, word, %Location{
-                  from: %{row: shift + 1, column: col + shift},
-                  to: %{row: upper_bound + 1, column: col + upper_bound}
-                })
-              else
-                acc
-              end
-            end)
-            |> Map.merge(search_results)
-          else
-          end
-        else
-          [isTopToBottom | [col | [element]]] =
-            Enum.reduce(vertical, [false, nil, nil], fn {index, element}, acc ->
-              if String.contains?(element, word), do: [true, index + 1, element], else: acc
-            end)
-
-          if isTopToBottom do
-            distance = String.length(element) - String.length(word)
-
-            Enum.reduce(0..distance, %{}, fn shift, acc ->
-              upper_bound = shift + (String.length(word) - 1)
-
-              if String.slice(element, shift..upper_bound) == word do
-                Map.put_new(acc, word, %Location{
-                  from: %{row: shift + 1, column: col},
-                  to: %{row: String.length(word) + shift, column: col}
-                })
-              else
-                acc
-              end
-            end)
-            |> Map.merge(search_results)
-          else
-            [isBottomToTop | [col | [element]]] =
-              Enum.reduce(vertical, [false, nil, nil], fn {index, element}, acc ->
-                if String.contains?(element, String.reverse(word)),
-                  do: [true, index + 1, element],
-                  else: acc
-              end)
-
-            if isBottomToTop do
-              distance = String.length(element) - String.length(word)
-
-              Enum.reduce(0..distance, %{}, fn shift, acc ->
-                upper_bound = shift + (String.length(word) - 1)
-
-                if String.slice(element, shift..upper_bound) == String.reverse(word) do
-                  Map.put_new(acc, word, %Location{
-                    from: %{row: String.length(word) + shift, column: col},
-                    to: %{row: shift + 1, column: col}
-                  })
-                else
-                  acc
-                end
-              end)
-              |> Map.merge(search_results)
-            end
-          end
-        end
-      else
-        [isLeftToRight | [row | [element]]] =
-          Enum.reduce(flattened_grid_map, [false, nil, nil], fn {index, element}, acc ->
-            if String.contains?(element, word), do: [true, index + 1, element], else: acc
-          end)
-
-        if isLeftToRight do
-          distance = String.length(element) - String.length(word)
-
-          Enum.reduce(0..distance, %{}, fn shift, acc ->
-            upper_bound = shift + (String.length(word) - 1)
-
-            if String.slice(element, shift..upper_bound) == word do
-              Map.put_new(acc, word, %Location{
-                from: %{row: row, column: shift + 1},
-                to: %{row: row, column: String.length(word) + shift}
-              })
-            else
-              acc
-            end
-          end)
-          |> Map.merge(search_results)
-        else
-          [isRightToLeft | [row | [element]]] =
-            Enum.reduce(flattened_grid_map, [false, nil, nil], fn {index, element}, acc ->
-              if String.contains?(element, String.reverse(word)),
-                do: [true, index + 1, element],
-                else: acc
-            end)
-
-          if isRightToLeft do
-            distance = String.length(element) - String.length(word)
-
-            Enum.reduce(0..distance, %{}, fn shift, acc ->
-              upper_bound = shift + (String.length(word) - 1)
-
-              if String.slice(element, shift..upper_bound) == String.reverse(word) do
-                Map.put_new(acc, word, %Location{
-                  to: %{row: row, column: shift + 1},
-                  from: %{row: row, column: String.length(word) + shift}
-                })
-              else
-                acc
-              end
-            end)
-            |> Map.merge(search_results)
-          else
-          end
-        end
+      if found? do
+        first = List.first(domain)
+        last = List.last(domain)
+        Location.new(first, last)
       end
     end)
   end
 end
-
-# grid = """
-# jefblpepre
-# camdcimgtc
-# oivokprjsm
-# pbwasqroua
-# rixilelhrs
-# wolcqlirpc
-# screeaumgr
-# alxhpburyi
-# jalaycalmp
-# clojurermt
-# """
-
-# "######################################################" |> IO.puts()
-
-# Enum.reduce(1..length(list), %{list: list, vals: []}, fn _, %{list: items, vals: vals} ->
-#   %{j: _, str: strval} =
-#     Enum.reduce(items, %{j: 0, str: ""}, fn element, %{j: j, str: str} ->
-#       %{j: j + 1, str: "#{str}#{String.at(element, j)}"}
-#     end)
-
-#   [_ | tail] = items
-#   %{list: tail, vals: vals ++ [strval]}
-# end)
-# |> IO.inspect()
